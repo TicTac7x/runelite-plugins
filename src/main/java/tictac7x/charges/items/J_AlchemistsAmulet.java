4ecfcd3a13e4c51e082d8b5c0061b14a180101b0
TicTac7x <tictac7x@gmail.com>
2025-01-24 03:04:37 +0200

    Cleanup

--- src/main/java/tictac7x/charges/items/J_AlchemistsAmulet.java
+++ src/main/java/tictac7x/charges/items/J_AlchemistsAmulet.java
@@ -16,38 +16,38 @@ import tictac7x.charges.store.Store;
 
 public class J_AlchemistsAmulet extends ChargedItem {
     public J_AlchemistsAmulet(
-            final Client client,
-            final ClientThread clientThread,
-            final ConfigManager configManager,
-            final ItemManager itemManager,
-            final InfoBoxManager infoBoxManager,
-            final ChatMessageManager chatMessageManager,
-            final Notifier notifier,
-            final TicTac7xChargesImprovedConfig config,
-            final Store store,
-            final Gson gson
+        final Client client,
+        final ClientThread clientThread,
+        final ConfigManager configManager,
+        final ItemManager itemManager,
+        final InfoBoxManager infoBoxManager,
+        final ChatMessageManager chatMessageManager,
+        final Notifier notifier,
+        final TicTac7xChargesImprovedConfig config,
+        final Store store,
+        final Gson gson
     ) {
         super(TicTac7xChargesImprovedConfig.alchemists_amulet, ItemID.ALCHEMISTS_AMULET, client, clientThread, configManager, itemManager, infoBoxManager, chatMessageManager, notifier, config, store, gson);
 
         this.items = new TriggerItem[]{
-                new TriggerItem(ItemID.ALCHEMISTS_AMULET).needsToBeEquipped(), //Uncharged 29988
-                new TriggerItem(ItemID.ALCHEMISTS_AMULET_29990).needsToBeEquipped(), //Charged
-                new TriggerItem(ItemID.ALCHEMISTS_AMULET_29992).needsToBeEquipped(), //???
+            new TriggerItem(ItemID.ALCHEMISTS_AMULET).needsToBeEquipped(), //Uncharged 29988
+            new TriggerItem(ItemID.ALCHEMISTS_AMULET_29990).needsToBeEquipped(), //Charged
+            new TriggerItem(ItemID.ALCHEMISTS_AMULET_29992).needsToBeEquipped(), //???
         };
 
         this.triggers = new TriggerBase[] {
-                // Check
-                new OnChatMessage("Your Alchemist's amulet has (?<charges>.+) charges left.").setDynamicallyCharges(),
+            // Check
+            new OnChatMessage("Your Alchemist's amulet has (?<charges>.+) charges left.").setDynamicallyCharges(),
 
-                // Charge
-                new OnChatMessage("You apply an additional .+ charges to your Alchemist's amulet. It now has (?<charges>.+) charges in total.").setDynamicallyCharges(),
-                new OnChatMessage("You apply (?<charges>.+) charges to your Alchemist's amulet.").setDynamicallyCharges(),
+            // Charge
+            new OnChatMessage("You apply an additional .+ charges to your Alchemist's amulet. It now has (?<charges>.+) charges in total.").setDynamicallyCharges(),
+            new OnChatMessage("You apply (?<charges>.+) charges to your Alchemist's amulet.").setDynamicallyCharges(),
 
-                // Uncharge
-                new OnChatMessage("You uncharge your Alchemist's amulet, regaining .+ amulets of chemistry in the process.").setFixedCharges(0),
+            // Uncharge
+            new OnChatMessage("You uncharge your Alchemist's amulet, regaining .+ amulets of chemistry in the process.").setFixedCharges(0),
 
-                // Use charge
-                new OnChatMessage("Your Alchemist's amulet helps you create a 4-dose potion. It has (?<charges>.+) charges left.").setDynamicallyCharges(),
+            // Use charge
+            new OnChatMessage("Your Alchemist's amulet helps you create a 4-dose potion. It has (?<charges>.+) charges left.").setDynamicallyCharges(),
         };
     }
 }
