30c508b49c14c8aa412f263d3db7119d44d8e7ea
kaptinkillem <travis.earley96@gmail.com>
2025-04-19 23:27:33 -0700

    removed consume

--- src/main/java/tictac7x/charges/items/U_PlankSack.java
+++ src/main/java/tictac7x/charges/items/U_PlankSack.java
@@ -93,25 +93,13 @@ public class U_PlankSack extends ChargedItemWithStorage {
 
             // Plank Make on Log in Inventory
             new OnXpDrop(Skill.MAGIC).onMenuOption("Cast").onMenuTarget(
-                    "Plank Make -> Logs").requiredItem(ItemID.PLANK_SACK).consumer(
-                    () -> {
-                        storage.add(ItemID.PLANK, 1);
-                    }),
+                    "Plank Make -> Logs").requiredItem(ItemID.PLANK_SACK).addToStorage(ItemID.PLANK, 1),
             new OnXpDrop(Skill.MAGIC).onMenuOption("Cast").onMenuTarget(
-                    "Plank Make -> Oak logs").requiredItem(ItemID.PLANK_SACK).consumer(
-                    () -> {
-                        storage.add(ItemID.OAK_PLANK, 1);
-                    }),
+                    "Plank Make -> Oak logs").requiredItem(ItemID.PLANK_SACK).addToStorage(ItemID.OAK_PLANK, 1),
             new OnXpDrop(Skill.MAGIC).onMenuOption("Cast").onMenuTarget(
-                    "Plank Make -> Teak logs").requiredItem(ItemID.PLANK_SACK).consumer(
-                    () -> {
-                        storage.add(ItemID.TEAK_PLANK, 1);
-                    }),
+                    "Plank Make -> Teak logs").requiredItem(ItemID.PLANK_SACK).addToStorage(ItemID.TEAK_PLANK, 1),
             new OnXpDrop(Skill.MAGIC).onMenuOption("Cast").onMenuTarget(
-                    "Plank Make -> Mahogany logs").requiredItem(ItemID.PLANK_SACK).consumer(
-                    () -> {
-                        storage.add(ItemID.MAHOGANY_PLANK, 1);
-                    }),
+                    "Plank Make -> Mahogany logs").requiredItem(ItemID.PLANK_SACK).addToStorage(ItemID.MAHOGANY_PLANK, 1),
 
             // Mahogany homes - 1 plank
             new OnXpDrop(Skill.CONSTRUCTION).xpAmountConsumer((xp) -> {
