670bb12823f62132cf3d05c9d599a9fa6ed1a429
TicTac7x <tictac7x@gmail.com>
2025-05-06 10:52:47 +0300

    Blood essence destroy support

--- src/main/java/tictac7x/charges/items/utils/U_BloodEssence.java
+++ src/main/java/tictac7x/charges/items/utils/U_BloodEssence.java
@@ -1,14 +1,15 @@
 package tictac7x.charges.items.utils;
 
+import net.runelite.api.widgets.Widget;
 import tictac7x.charges.TicTac7xChargesImprovedConfig;
+import tictac7x.charges.TicTac7xChargesImprovedPlugin;
 import tictac7x.charges.item.ChargedItemWithStatus;
-import tictac7x.charges.item.triggers.OnChatMessage;
-import tictac7x.charges.item.triggers.OnItemContainerChanged;
-import tictac7x.charges.item.triggers.TriggerBase;
-import tictac7x.charges.item.triggers.TriggerItem;
+import tictac7x.charges.item.triggers.*;
 import tictac7x.charges.store.ids.ItemContainerId;
 import tictac7x.charges.store.ids.ItemId;
 import tictac7x.charges.store.Provider;
+
+import java.util.Optional;
 
 public class U_BloodEssence extends ChargedItemWithStatus {
     public U_BloodEssence(final Provider provider) {
@@ -27,10 +28,10 @@ public class U_BloodEssence extends ChargedItemWithStatus {
             new OnChatMessage("You manage to extract power from the Blood Essence and craft (?<charges>.+) extra runes?.").decreaseDynamicallyCharges(),
 
             // Depleted.
-            new OnChatMessage("Your blood essence falls apart, drained of energy.").setFixedCharges(0).deactivate(),
+            new OnChatMessage("Your blood essence falls apart, drained of energy.").setFixedCharges(1000).deactivate(),
 
             // Activate.
-            new OnChatMessage("You activate the blood essence.").setFixedCharges(1000).activate(),
+            new OnChatMessage("You activate the blood essence.").activate(),
 
             // Status from inventory.
             new OnItemContainerChanged(ItemContainerId.INVENTORY).itemsConsumer(items -> {
@@ -38,6 +39,23 @@ public class U_BloodEssence extends ChargedItemWithStatus {
                     activate();
                 } else if (items.hasItem(ItemId.BLOOD_ESSENCE_INACTIVE)) {
                     deactivate();
+                }
+            }),
+
+            // Destroy.
+            new OnScriptPreFired(1651).scriptConsumer((script) -> {
+                final Optional<Widget> destroyWidgetItem = TicTac7xChargesImprovedPlugin.getWidget(provider.client, 584, 5);
+                if (
+                    destroyWidgetItem.isPresent() &&
+                    (destroyWidgetItem.get().getItemId() == ItemId.BLOOD_ESSENCE_ACTIVE || destroyWidgetItem.get().getItemId() == ItemId.BLOOD_ESSENCE_INACTIVE) &&
+                    script.getScriptEvent().getArguments().length >= 5 &&
+                    script.getScriptEvent().getArguments()[4].toString().equals("Yes")
+                ) {
+                    provider.store.addConsumerToNextTickQueue(() -> setCharges(1000));
+
+                    if (destroyWidgetItem.get().getItemId() == ItemId.BLOOD_ESSENCE_ACTIVE) {
+                        provider.store.addConsumerToNextTickQueue(this::deactivate);
+                    }
                 }
             }),
         };
