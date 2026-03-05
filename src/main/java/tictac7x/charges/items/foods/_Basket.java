64b4d3f26ee6aef7cac77a3540bc4411430fc1e1
TicTac7x <tictac7x@gmail.com>
2026-03-05 12:16:31 +0200

    Simplify

--- src/main/java/tictac7x/charges/items/foods/_Basket.java
+++ src/main/java/tictac7x/charges/items/foods/_Basket.java
@@ -5,8 +5,6 @@ import tictac7x.charges.item.ChargedItem;
 import tictac7x.charges.item.triggers.TriggerItem;
 import tictac7x.charges.store.Provider;
 
-import java.awt.*;
-
 public class _Basket extends ChargedItem {
     public _Basket(
         final String configKey,
@@ -20,26 +18,5 @@ public class _Basket extends ChargedItem {
     @Override
     public String getConfigKey() {
         return TicTac7xChargesImprovedConfig.baskets;
-    }
-
-    @Override
-    public Color getTextColor(final int itemId) {
-        for (final TriggerItem triggerItem : items) {
-            if (triggerItem.itemId == itemId && triggerItem.fixedCharges.isPresent()) {
-                switch (triggerItem.fixedCharges.get()) {
-                    case 5:
-                    case 4:
-                        return provider.config.get4DoseColor();
-                    case 3:
-                        return provider.config.get3DoseColor();
-                    case 2:
-                        return provider.config.get2DoseColor();
-                    case 1:
-                        return provider.config.get1DoseColor();
-                }
-            }
-        }
-
-        return super.getTextColor(itemId);
     }
 }
--- src/main/java/tictac7x/charges/items/foods/_Sack.java
+++ src/main/java/tictac7x/charges/items/foods/_Sack.java
@@ -5,8 +5,6 @@ import tictac7x.charges.item.ChargedItem;
 import tictac7x.charges.item.triggers.TriggerItem;
 import tictac7x.charges.store.Provider;
 
-import java.awt.*;
-
 public class _Sack extends ChargedItem {
     public _Sack(
         final String configKey,
@@ -20,32 +18,5 @@ public class _Sack extends ChargedItem {
     @Override
     public String getConfigKey() {
         return TicTac7xChargesImprovedConfig.baskets;
-    }
-
-    @Override
-    public Color getTextColor(final int itemId) {
-        for (final TriggerItem triggerItem : items) {
-            if (triggerItem.itemId == itemId && triggerItem.fixedCharges.isPresent()) {
-                switch (triggerItem.fixedCharges.get()) {
-                    case 10:
-                    case 9:
-                    case 8:
-                        return provider.config.get4DoseColor();
-                    case 7:
-                    case 6:
-                    case 5:
-                        return provider.config.get3DoseColor();
-                    case 4:
-                    case 3:
-                        return provider.config.get2DoseColor();
-                    case 2:
-                    case 1:
-                        return provider.config.get1DoseColor();
-
-                }
-            }
-        }
-
-        return super.getTextColor(itemId);
     }
 }
