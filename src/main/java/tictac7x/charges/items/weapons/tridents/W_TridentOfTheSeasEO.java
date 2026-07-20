c362458714dd70179c3ec6cec25de5323a39ab47
wukx <w@wukx.net>
2026-07-20 11:14:48 +0200

    Add support for TOTS & Toxic TOTS demonic ornament variants

---
+++ src/main/java/tictac7x/charges/items/weapons/tridents/W_TridentOfTheSeasEO.java
@@ -0,0 +1,21 @@
+package tictac7x.charges.items.weapons.tridents;
+
+import net.runelite.api.gameval.ItemID;
+import tictac7x.charges.TicTac7xChargesImprovedConfig;
+import tictac7x.charges.store.Provider;
+
+import java.util.Optional;
+
+public class W_TridentOfTheSeasEO extends _Trident {
+    public W_TridentOfTheSeasEO(Provider provider) {
+        super(
+            TicTac7xChargesImprovedConfig.trident_of_the_seas_e_o,
+            ItemID.TOTS_I_CHARGED_ORN,
+            ItemID.TOTS_I_UNCHARGED_ORN,
+            Optional.empty(),
+            "Trident of the seas (e) (o)",
+            3721,
+            provider
+        );
+    }
+}
