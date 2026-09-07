aa7ddf61b5ae822a58dd9b7379b28fa2d9eb2d29
minorpains <aunder6235@gmail.com>
2026-09-07 15:30:20 -0500

    Update vein reeset logic

--- src/main/java/tictac7x/motherlode/oreveins/OreVein.java
+++ src/main/java/tictac7x/motherlode/oreveins/OreVein.java
@@ -11,17 +11,22 @@ import tictac7x.motherlode.Character;
 import tictac7x.motherlode.sectors.Sector;
 import tictac7x.motherlode.TicTac7xMotherlodeConfig;
 import java.awt.Color;
+import java.util.List;
 
 public class OreVein {
     public final int x;
     public final int y;
-    public final Sector sector;
+    public final List<Sector> sector;
 
     private boolean isDepleted;
     private boolean isDepleting;
-    private int regenerationTicks = 0;
+    private boolean isResetting;
+    private int ticksTowardsReset = 0;
     private float health;
-    private final int RESPAWN_TIME_GAMETICKS = 100;
+    private final int RESPAWN_LONG_TIME_GAMETICKS = 170; // max observed respawn time on long reset nodes
+    private final int RESPAWN_SHORT_TIME_GAMETICKS = 100; // max observed respawn time on short reset nodes
+    private final int RESET_LONG_MAX_TIME_GAMETICKS =  220; //  time for some downstairs veins to reset if player stops mining before depletion
+    private final int RESET_SHORT_NAX_TIME_GAMETICKS = 160;  // max observed time for all other veins to reset if player stops mining before depletion
     private final int DESPAWN_TIME_DOWNSTAIRS_GAMETICKS = 45;
     private final int DESPAWN_TIME_UPPERFLOOR_GAMETICKS = 67;
     private final Provider provider;
@@ -33,7 +38,7 @@ public class OreVein {
         this.x = x;
         this.y = y;
         this.isDepleted = isDepleted;
-        this.sector = Sectors.getSectors(x, y, false).get(0);
+        this.sector = Sectors.getSectors(x, y, true);
         this.health = getMaxHealth();
         this.provider = provider;
     }
@@ -44,39 +49,58 @@ public class OreVein {
         this.isDepleted = isDepleted;
         this.health = isDepleted ? 0 : getMaxHealth();
         this.isDepleting = false;
-        regenerationTicks = 0;
+        this.isResetting = false;
+        ticksTowardsReset = 0;
     }
 
     public void startDepleting() {
         isDepleting = true;
-        regenerationTicks = 0;
     }
 
-    public void startRegenerating() {
-        regenerationTicks++;
-
-        if (regenerationTicks > 2) {
-            isDepleting = false;
+    public void startResetting() {
+        ticksTowardsReset++;
+        if (ticksTowardsReset > 2) {
+            isResetting = true;
         }
+    }
+
+    public void stopResetting() {
+        ticksTowardsReset = 0;
+        isResetting = false;
+    }
+
+    public void resetCompleted() {
+        stopResetting();
+        isDepleting = false;
     }
 
     public void onGameTick() {
         final boolean isBeingMined = isBeingMined();
 
         if (isDepleted) {
-            health += (getMaxHealth()) / RESPAWN_TIME_GAMETICKS;
+            health += (getMaxHealth()) / getRespawnTime();
         } else if (isDepleting) {
             health = Math.max(health - 1, 0);
-        } else if (regenerationTicks > 2) {
-            health = Math.min(health + 1, getMaxHealth());
         }
 
-        if (!isDepleted && isBeingMined) {
-            startDepleting();
-        }
-
-        if (!isDepleted && !isBeingMined) {
-            startRegenerating();
+        if (!isDepleted) {
+            if (isBeingMined) {
+                if (!isDepleting) {
+                    startDepleting();
+                } else if (isResetting) {
+                    stopResetting();
+                }
+            } else {
+                if (!isResetting) {
+                    startResetting();
+                } else {
+                    ticksTowardsReset++;
+                    if (ticksTowardsReset >= getResetTime()) {
+                        health = getMaxHealth();
+                        resetCompleted();
+                    }
+                }
+            }
         }
     }
 
@@ -92,11 +116,11 @@ public class OreVein {
     }
 
     public boolean isRendering(final TicTac7xMotherlodeConfig config, final Character character) {
-        if (config.upstairsOnly() && sector == Sector.DOWNSTAIRS) {
+        if (config.upstairsOnly() && sector.contains(Sector.DOWNSTAIRS)) {// sector == Sector.DOWNSTAIRS) {
             return false;
         }
 
-        if (!character.getSectors().contains(sector)) {
+        if (!character.getSectors().contains(sector.get(0))) {
             return false;
         }
 
@@ -104,9 +128,21 @@ public class OreVein {
     }
 
     private float getMaxHealth() {
-        return sector == Sector.DOWNSTAIRS
+        return sector.contains(Sector.DOWNSTAIRS)  //sector == Sector.DOWNSTAIRS
             ? DESPAWN_TIME_DOWNSTAIRS_GAMETICKS
             : DESPAWN_TIME_UPPERFLOOR_GAMETICKS;
+    }
+
+    private float getRespawnTime() {
+        return sector.contains(Sector.LONG_RESET_SECTOR)
+            ? RESPAWN_LONG_TIME_GAMETICKS
+            : RESPAWN_SHORT_TIME_GAMETICKS;
+    }
+
+    private float getResetTime() {
+        return sector.contains(Sector.LONG_RESET_SECTOR) //sector == Sector.DOWNSTAIRS
+             ? RESET_LONG_MAX_TIME_GAMETICKS
+             : RESET_SHORT_NAX_TIME_GAMETICKS;
     }
 
     public boolean isBeingMined() {
